0a531eff7bce8daed4928d5071e9e1649e7a118a
amadau <amadau@air15.local>
2025-07-24 19:04:35 +0700

    setting for pushing to s3

--- game.py
+++ game.py
@@ -236,19 +236,21 @@ class GameManager:
             content = "\n".join(line.hidden + "\n" + line.visible for line in sheet)
             with open(path, "w", encoding="utf-8") as f:
                 f.write(content)
-            upload_to_s3(room_id, room.session_time, room.players[i].name, content)
+            if os.getenv("PUSH_TO_S3") == "YES":
+                upload_to_s3(room_id, room.session_time, room.players[i].name, content)
 
 
 # экземпляр менеджера
 manager = GameManager()
 
-session = boto3.session.Session()
-s3 = session.client(
-    service_name='s3',
-    endpoint_url=os.getenv("S3_ENDPOINT"),
-    aws_access_key_id=os.getenv("S3_ACCESS_KEY"),
-    aws_secret_access_key=os.getenv("S3_SECRET_KEY")
-)
+if os.getenv("PUSH_TO_S3") == "YES":
+    session = boto3.session.Session()
+    s3 = session.client(
+        service_name='s3',
+        endpoint_url=os.getenv("S3_ENDPOINT"),
+        aws_access_key_id=os.getenv("S3_ACCESS_KEY"),
+        aws_secret_access_key=os.getenv("S3_SECRET_KEY")
+    )
 
 def upload_to_s3(room_id, session_time, player_name, content):
     key = f"{room_id}/{session_time}/{player_name}.txt"
